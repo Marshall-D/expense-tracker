224b4f43046d9448e01dccccf96e406becd4b8f6
d1 <www.didowu@gmail.com>
2023-05-21 12:23:39 +0100

    added dynamic array

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,54 +1,32 @@
 import { useState } from "react";
 
-import ExpenseItem  from "./ExpenseItem";
-import './Expenses.css';
+import ExpenseItem from "./ExpenseItem";
+import "./Expenses.css";
 import Card from "../UI/Card";
 import ExpensesFilter from "../NewExpense/ExpensesFilter";
 
-function Expenses(props){
+function Expenses(props) {
   const [enteredYear, setEnteredYear] = useState("");
 
-
-
-   
   const saveYearDataHandler = (enteredYearData) => {
-    
     console.log("we are in expenses");
     console.log(enteredYearData);
     setEnteredYear(enteredYearData);
     console.log(enteredYear);
-
-
   };
-    return(
-      <div>
-        <ExpensesFilter onSaveYearData={saveYearDataHandler}/>
-        <Card className="expenses">
-        <ExpenseItem
-        title={props.expenses[0].title}
-        amount={props.expenses[0].amount}
-        date={props.expenses[0].date}
-      />
-      <ExpenseItem 
-      title={props.expenses[1].title}
-      amount={props.expenses[1].amount}
-      date={props.expenses[1].date}
-      />
-      <ExpenseItem 
-      title={props.expenses[2].title}
-      amount={props.expenses[2].amount}
-      date={props.expenses[2].date}
-      />
-      <ExpenseItem 
-      title={props.expenses[3].title}
-      amount={props.expenses[3].amount}
-      date={props.expenses[3].date}/>
-    </Card>
-
+  return (
+    <div>
+      <ExpensesFilter onSaveYearData={saveYearDataHandler} />
+      <Card className="expenses">
+        {props.expenses.map((expenses) => (
+          <ExpenseItem
+            title={expenses.title}
+            amount={expenses.amount}
+            date={expenses.date}
+          />
+        ))}
+      </Card>
     </div>
-
-    );
-
-
+  );
 }
 export default Expenses;
