109b9e0e8ea074effc9f85d41a2620d57df82139
d1 <www.didowu@gmail.com>
2023-05-22 08:22:40 +0100

    refcfored code

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -3,36 +3,29 @@ import { useState } from "react";
 import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
 import Card from "../UI/Card";
-import ExpensesFilter from "../NewExpense/ExpensesFilter";
+import ExpensesFilter from "./ExpensesFilter";
+import ExpensesList from "./ExpensesList";
 
 function Expenses(props) {
-  const [enteredYear, setEnteredYear] = useState(2020);
-  
+  const [enteredYear, setEnteredYear] = useState("2020");
 
   const saveYearDataHandler = (enteredYearData) => {
     // console.log("we are in expenses");
     console.log(enteredYearData);
     setEnteredYear(enteredYearData);
-
-
-
-};
-    const expenses = props.expenses.filter(expenses => expenses.date.getFullYear().toString() === enteredYear);
-
+  };
+  const expenses = props.expenses.filter(
+    (expenses) => expenses.date.getFullYear().toString() === enteredYear
+  );
 
   return (
     <div>
       <Card className="expenses">
-      <ExpensesFilter selected={enteredYear} onSaveYearData={saveYearDataHandler} />
-
-        {expenses.map((expenses) => (
-          <ExpenseItem
-          key={expenses.id}
-            title={expenses.title}
-            amount={expenses.amount}
-            date={expenses.date}
-          />
-        ))}
+        <ExpensesFilter
+          selected={enteredYear}
+          onSaveYearData={saveYearDataHandler}
+        />
+      <ExpensesList expenses={expenses}/>
       </Card>
     </div>
   );
---
+++ src/components/Expenses/ExpensesList.js
@@ -0,0 +1,22 @@
+import ExpenseItem from "./ExpenseItem";
+import "./ExpensesList.css";
+
+const ExpensesList = (props) => {
+  if (props.expenses.length === 0) {
+    return <h2 className="expenses-list__fallback"> Found No expenses</h2>;
+  }
+
+  return (
+    <ul className="expenses_list">
+      {props.expenses.map((expenses) => (
+        <ExpenseItem
+          key={expenses.id}
+          title={expenses.title}
+          amount={expenses.amount}
+          date={expenses.date}
+        />
+      ))}
+    </ul>
+  );
+};
+export default ExpensesList;
